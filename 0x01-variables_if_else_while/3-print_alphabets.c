e929ec5e2b6ea30bfbadb90df7b5440186bdc8f4
Firaol-Dagne <firaoldagne@gmail.com>
2022-09-09 13:33:18 -0700

    Task_4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - void
+ *
+ * Return: returns zero
+ */
+int main(void)
+{
+	char letter;
+	for(letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for(letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+
+}
+
