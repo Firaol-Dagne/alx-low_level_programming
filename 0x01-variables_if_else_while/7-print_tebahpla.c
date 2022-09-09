2b0f5c73b215ce763ffbedf614598a168d1cfed2
Firaol-Dagne <firaoldagne@gmail.com>
2022-09-09 14:14:07 -0700

    Task_8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - void
+ *
+ * Return: returns zero
+ */
+int main(void)
+{
+	char letter;
+
+		for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+	
+	return (0);
+
+}
+
