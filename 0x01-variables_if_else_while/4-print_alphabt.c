0a9cec92d997aa9aaf1beb13a7868160d89c3e32
Firaol-Dagne <firaoldagne@gmail.com>
2022-09-09 13:51:12 -0700

    Task_5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - void
+ *
+ * Return: returns zero
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
