e159a6962a417ac1d8b8b80142aaeed787cba7c0
Firaol-Dagne <firaoldagne@gmail.com>
2022-09-13 14:48:30 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _abs - absolute value
+ * @n: abolute
+ * Return: zero
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+
+}
