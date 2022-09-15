890e4112e342aa4826f98a598f31241c9ece8e44
Firaol-Dagne <firaoldagne@gmail.com>
2022-09-15 12:40:37 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - list the upper case
+ *
+ * @c: parametres
+ *
+ * Return: zero
+ */
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+
+}
+
