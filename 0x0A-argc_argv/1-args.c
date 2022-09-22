ee726e9a92efcbe280387d942cca5d888c3b21aa
Firaol-Dagne <firaoldagne85@gmail.com>
2022-09-22 22:47:07 +0000

    task4

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: int
+ * @argv: list
+ * Return: zero
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
