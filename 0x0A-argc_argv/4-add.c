865b18bafe7a4fb5fa856996e62da252b0ae0793
Firaol-Dagne <firaoldagne85@gmail.com>
2022-09-23 01:28:47 +0000

    task6

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,15 +8,14 @@
 
 int isInteger(const char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (1);
-		i++;
-	}
-	return (0);
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
+		return (1);
+	i++;
+}
+return (0);
 }
 
 /**
@@ -28,18 +27,18 @@ int isInteger(const char *s)
 
 int main(int argc, char const *argv[])
 {
-	int sum = 0;
-
-	while (--argc)
+int sum = 0;
+while (--argc)
+{
+	if (isInteger(argv[argc]))
 	{
-		if (isInteger(argv[argc]))
-		{
-			printf("Error\n");
-			return (1);	
-		}
-		sum += atoi(argv[argc]);
+		printf("Error\n");
+		return (1);	
 	}
+	sum += atoi(argv[argc]);
+}
 	
-	printf("%i\n", sum);
-	return (0);
+printf("%i\n", sum);
+
+return (0);
 }
