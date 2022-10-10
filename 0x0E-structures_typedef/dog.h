9c633b43e9d9a6dc12900e365c6ec087b3fe79a8
Firaol-Dagne <firaoldagne85@gmail.com>
2022-10-10 18:40:31 +0000

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
 typedef struct dog
 {
 
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 
 } dog_t;
+
+
+
+
+
+#endif
