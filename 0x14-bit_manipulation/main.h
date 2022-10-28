7eed3269faee2be2ff70d94923905ae45a7eb2f4
Firaol-Dagne <firaoldagne85@gmail.com>
2022-10-28 16:51:36 +0000

    task1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+/*
+ * File: holberton.h
+ * AUth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x13-bit_manipulation directory.
+ */
+
+
+
 int _strlen(const char *s);
 
 int _putchar(char c);
